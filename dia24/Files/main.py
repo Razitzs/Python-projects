d428263357bbfffff40b8131eed330461e1c9e31
Razitzs <fer.razo@hotmail.com>
2022-07-08 19:09:16 -0500

    Dia 25 final

--- dia24/Files/main.py
+++ dia24/Files/main.py
@@ -1,8 +1,14 @@
-# with open("dia24/Files/file.txt") as file:
-#     contents=file.read()
-#     print (contents)
+PLACEHOLDER = "[name]"
 
 
-with open("dia24/Files/file.txt",mode="r") as file:
-    for a in file.readlines():
-        print(a)
+with open("dia24/Files/Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("dia24/Files/Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"dia24/Files/Output/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+
